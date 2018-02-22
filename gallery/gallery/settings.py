cd7ea525fbe92b78e89da3cb145802b1a32c858c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-23 01:56:11 +0300

    Added Image models and configured db

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'sam',
+        'PASSWORD': 'Sam'
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- gallery/index/models.py
+++ gallery/index/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/')
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    # location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
